34ccd48b0c876addcf6f88e1b8d275b1b120d9b4
doylet <doyle.github.2ifxi@simplelogin.com>
2024-06-14 17:13:42 +1000

    Do not skip 1st stack frame (the callsite) when dumping trace

--- src/common/exception.h
+++ src/common/exception.h
@@ -29,7 +29,7 @@ void on_terminate_handler();
 struct exception : public std::exception {
     exception(std::string&& msg,
             cpptrace::raw_trace&& trace =
-                    cpptrace::generate_raw_trace(/*skip*/ 1, /*max_depth*/ 256));
+                    cpptrace::generate_raw_trace(/*skip*/ 0, /*max_depth*/ 256));
 
     const char* what() const noexcept override;
 
@@ -49,63 +49,63 @@ struct logic_error : public exception {
     logic_error(
             std::string msg,
             cpptrace::raw_trace&& trace =
-                    cpptrace::generate_raw_trace(/*skip*/ 1, /*max_depth*/ 256)) noexcept;
+                    cpptrace::generate_raw_trace(/*skip*/ 0, /*max_depth*/ 256)) noexcept;
 };
 
 struct domain_error : public exception {
     domain_error(
             std::string msg,
             cpptrace::raw_trace&& trace =
-                    cpptrace::generate_raw_trace(/*skip*/ 1, /*max_depth*/ 256)) noexcept;
+                    cpptrace::generate_raw_trace(/*skip*/ 0, /*max_depth*/ 256)) noexcept;
 };
 
 struct invalid_argument : public exception {
     invalid_argument(
             std::string msg,
             cpptrace::raw_trace&& trace =
-                    cpptrace::generate_raw_trace(/*skip*/ 1, /*max_depth*/ 256)) noexcept;
+                    cpptrace::generate_raw_trace(/*skip*/ 0, /*max_depth*/ 256)) noexcept;
 };
 
 struct length_error : public exception {
     length_error(
             std::string msg,
             cpptrace::raw_trace&& trace =
-                    cpptrace::generate_raw_trace(/*skip*/ 1, /*max_depth*/ 256)) noexcept;
+                    cpptrace::generate_raw_trace(/*skip*/ 0, /*max_depth*/ 256)) noexcept;
 };
 
 struct out_of_range : public exception {
     out_of_range(
             std::string msg,
             cpptrace::raw_trace&& trace =
-                    cpptrace::generate_raw_trace(/*skip*/ 1, /*max_depth*/ 256)) noexcept;
+                    cpptrace::generate_raw_trace(/*skip*/ 0, /*max_depth*/ 256)) noexcept;
 };
 
 struct runtime_error : public exception {
     runtime_error(
             std::string msg,
             cpptrace::raw_trace&& trace =
-                    cpptrace::generate_raw_trace(/*skip*/ 1, /*max_depth*/ 256)) noexcept;
+                    cpptrace::generate_raw_trace(/*skip*/ 0, /*max_depth*/ 256)) noexcept;
 };
 
 struct range_error : public exception {
     range_error(
             std::string msg,
             cpptrace::raw_trace&& trace =
-                    cpptrace::generate_raw_trace(/*skip*/ 1, /*max_depth*/ 256)) noexcept;
+                    cpptrace::generate_raw_trace(/*skip*/ 0, /*max_depth*/ 256)) noexcept;
 };
 
 struct overflow_error : public exception {
     overflow_error(
             std::string msg,
             cpptrace::raw_trace&& trace =
-                    cpptrace::generate_raw_trace(/*skip*/ 1, /*max_depth*/ 256)) noexcept;
+                    cpptrace::generate_raw_trace(/*skip*/ 0, /*max_depth*/ 256)) noexcept;
 };
 
 struct underflow_error : public exception {
     underflow_error(
             std::string msg,
             cpptrace::raw_trace&& trace =
-                    cpptrace::generate_raw_trace(/*skip*/ 1, /*max_depth*/ 256)) noexcept;
+                    cpptrace::generate_raw_trace(/*skip*/ 0, /*max_depth*/ 256)) noexcept;
 };
 
 struct system_error : public exception {
@@ -113,7 +113,7 @@ struct system_error : public exception {
             int error_code,
             std::string msg,
             cpptrace::raw_trace&& trace =
-                    cpptrace::generate_raw_trace(/*skip*/ 1, /*max_depth*/ 256)) noexcept;
+                    cpptrace::generate_raw_trace(/*skip*/ 0, /*max_depth*/ 256)) noexcept;
 
     const std::error_code& code() const noexcept;
 
