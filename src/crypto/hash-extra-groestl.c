f3d81e12366d71d9ee40dc38888d0843fb46c3c9
Jason Rhinelander <jason@imaginary.ca>
2024-09-09 19:23:20 -0300

    Fix function pointer arg type mismatch
    Yay C!

--- src/crypto/hash-extra-groestl.c
+++ src/crypto/hash-extra-groestl.c
@@ -33,6 +33,6 @@
 
 #include "groestl.h"
 
-void hash_extra_groestl(const void* data, size_t length, char* hash) {
-    groestl(data, length * 8, (uint8_t*)hash);
+void hash_extra_groestl(const void* data, size_t length, unsigned char* hash) {
+    groestl(data, length * 8, hash);
 }
