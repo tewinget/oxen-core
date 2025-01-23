e017c36885008ba4a86e981641d325492b0ad705
Thomas Winget <tewinget@gmail.com>
2025-01-22 22:20:57 -0500

    add hf21 dry run rpc method
    calling this method returns a before and after as if hf21 would
    happen the very next block.  Because outstanding oxen rewards will
    differ from the exact fork height (and from block to block), this
    can only be approximate, but it should be fairly close to what the
    actual result will be.

--- src/cryptonote_core/service_node_list.h
+++ src/cryptonote_core/service_node_list.h
@@ -1278,6 +1278,13 @@ class service_node_list {
 
     cryptonote::Blockchain& blockchain;
 
+    struct hf21_transition_result {
+      service_nodes_infos_t sns_after;
+      std::pair<std::vector<std::string>, std::vector<uint64_t>> rewards_after;
+    };
+
+    hf21_transition_result hf21_dry_run(cryptonote::network_type nettype) const;
+
   private:
     bool m_rescanning = false; /* set to true when doing a rescan so we know not to reset proofs */
     void process_block(
--- src/rpc/core_rpc_server.h
+++ src/rpc/core_rpc_server.h
@@ -159,6 +159,7 @@ class core_rpc_server {
     void invoke(SYNC_INFO& sync, rpc_context context);
     void invoke(GET_SERVICE_NODE_STATUS& sns, rpc_context context);
     void invoke(GET_SERVICE_NODES& sns, rpc_context context);
+    void invoke(HF21_DRY_RUN& req, rpc_context context);
     void invoke(GET_PENDING_EVENTS& sns, rpc_context context);
     void invoke(GET_LIMIT& limit, rpc_context context);
     void invoke(SET_LIMIT& limit, rpc_context context);
