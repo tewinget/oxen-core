83e1e2fbccbe34762df0ab7a8bbb7d881b56d847
Thomas Winget <tewinget@gmail.com>
2025-01-30 17:24:00 -0500

    devnet hf20 height

--- src/cryptonote_basic/hardfork.cpp
+++ src/cryptonote_basic/hardfork.cpp
@@ -151,8 +151,8 @@ static constexpr std::array devnet_hard_forks = {
         hard_fork{hf::hf14_blink, 0, 1, 1716310014},
         hard_fork{hf::hf16_pulse, 0, 500, 1716310016},
         hard_fork{hf::hf19_reward_batching, 0, 630, 1716310019},
-        hard_fork{hf::hf19_reward_batching, 1, 1'000'000'000, 1716310019},
-        hard_fork{hf::hf20_eth_transition, 0, 1'000'000'001, 1716310020},
+        hard_fork{hf::hf19_reward_batching, 1, 670, 1716310019},
+        hard_fork{hf::hf20_eth_transition, 0, 8400, 1716310020},
         hard_fork{hf::hf21_eth, 0, 1'000'000'002, 1716310021},
 };
 
