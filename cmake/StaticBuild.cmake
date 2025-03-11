e19a0934cadb53678e2f2f63a99a9381d7d982d8
Thomas Winget <tewinget@gmail.com>
2025-03-11 14:41:49 -0400

    boost static hard code cxx and cxxflags

--- cmake/StaticBuild.cmake
+++ cmake/StaticBuild.cmake
@@ -428,7 +428,7 @@ build_external(boost
     cp tools/build/src/engine/b2 .
   INSTALL_COMMAND
     ./b2 -d0 variant=release link=static runtime-link=static optimization=speed ${boost_extra}
-      threading=multi threadapi=${boost_threadapi} ${boost_buildflags} cxxstd=17 visibility=global
+      threading=multi threadapi=${boost_threadapi} ${boost_buildflags} cxxstd=17 visibility=global --cxx=/usr/lib/android-ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi21-clang++ --cxxflags="${CMAKE_CXX_FLAGS}"
       --disable-icu --user-config=${CMAKE_CURRENT_BINARY_DIR}/user-config.bjam --toolset=${boost_toolset} --verbose
       --prefix=${DEPS_DESTDIR} --exec-prefix=${DEPS_DESTDIR} --libdir=${DEPS_DESTDIR}/lib --includedir=${DEPS_DESTDIR}/include
       --with-program_options --with-system --with-thread --with-serialization --layout=system
